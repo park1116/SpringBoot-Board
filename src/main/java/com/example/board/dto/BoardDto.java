56d4ee03c69e7a367539eb687a77dbb3a0b717f1
park1116 <park_1116@naver.com>
2020-09-17 21:15:01 +0900

    DBconnect and initial setting

---
+++ src/main/java/com/example/board/dto/BoardDto.java
@@ -0,0 +1,41 @@
+package com.example.board.dto;
+
+import java.sql.Date;
+
+public class BoardDto {
+	private int num;
+	private String title;
+	private String content;
+	private String name;
+	private Date regdate;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+}
