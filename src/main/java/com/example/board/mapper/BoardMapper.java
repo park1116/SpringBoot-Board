c733b120a0cdddf885315fa3db5c35ebdd6c74a7
park1116 <park_1116@naver.com>
2020-09-21 22:15:54 +0900

    mapper생성 및 CRUD테스트

---
+++ src/main/java/com/example/board/mapper/BoardMapper.java
@@ -0,0 +1,20 @@
+package com.example.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.board.dto.BoardDto;
+
+@Mapper
+public interface BoardMapper {
+	public List<BoardDto> selectAll();
+	
+	public int insertData(BoardDto dto);
+	
+	public BoardDto selectOne(int num);
+	
+	public int updateData(BoardDto dto);
+	
+	public int deleteData(int num);
+}
