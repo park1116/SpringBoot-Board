7852aadd95618ce2c116ffc0d5e5fd5ccac4b798
park1116 <park_1116@naver.com>
2020-09-21 22:52:55 +0900

    jsp사용하기 위한 설정 및 test화면 띄우기

---
+++ src/main/java/com/example/board/controller/Test.java
@@ -0,0 +1,13 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+	@RequestMapping("/")
+	public String hello() {
+		return "test";
+	}
+}
